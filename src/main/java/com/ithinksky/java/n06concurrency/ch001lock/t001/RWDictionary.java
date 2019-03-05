77337f7f9e50921b2cdf0b4b68ac00a961fc5b0d
浮云晓生 <tengpeng.gao@gmail.com>
2019-03-05 22:18:45 +0800

    ReentrantReadWriteLock 读写锁基础运用

---
+++ src/main/java/com/ithinksky/java/n06concurrency/ch001lock/t001/RWDictionary.java
@@ -0,0 +1,58 @@
+package com.ithinksky.java.n06concurrency.ch001lock.t001;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 读写锁应用
+ *
+ * @author tengpeng.gao
+ * @since 2019/3/5
+ */
+public class RWDictionary {
+
+    private Map<String, Object> map = new HashMap<>();
+    private ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    private Lock readLock = rwl.readLock();
+    private Lock writeLock = rwl.writeLock();
+
+    public Object get(String key) {
+        readLock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public String[] getAllKeys() {
+        readLock.lock();
+        try {
+            return (String[]) map.entrySet().toArray();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public Object put(String key, Object value) {
+        writeLock.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public void clear() {
+        writeLock.lock();
+        try {
+            map.clear();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+
+}
