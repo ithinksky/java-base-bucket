63f17c1faaa5d4bd17acd6678946e26d7a8e1b90
浮云晓生 <tengpeng.gao@gmail.com>
2019-04-02 23:00:42 +0800

    数据结构的基本操作标准定义

--- src/main/java/com/ithinksky/datastructure/ch001linkedlist/Test001SingleLinkedList.java
+++ src/main/java/com/ithinksky/datastructure/ch001linkedlist/Test001SingleLinkedList.java
@@ -1,5 +1,7 @@
 package com.ithinksky.datastructure.ch001linkedlist;
 
+import com.ithinksky.datastructure.ICommonDataOperate;
+
 /**
  * 功能描述：单链表的定义及相关操作
  *
@@ -8,34 +10,79 @@ package com.ithinksky.datastructure.ch001linkedlist;
  */
 public class Test001SingleLinkedList {
 
+    public static class MySingleLinkedList<E> implements ICommonDataOperate<E> {
 
-    public static class MySingleLinkedList<E> {
-
+        // 头节点
         Node<E> first;
+
+        // 尾节点
+        Node<E> last;
+
+        public MySingleLinkedList() {
+            // 默认为空链表
+        }
+
+        public MySingleLinkedList(int length) {
+            // 初始化 length 长度的链表，节点内容为 null
+            for (int i = 1; i <= length; i++) {
+                this.add(null);
+            }
+        }
 
         private static class Node<E> {
             E item;
-            MySingleLinkedList.Node<E> next;
+            Node<E> next;
 
-            Node(E element, MySingleLinkedList.Node<E> next) {
+            Node(E element, Node<E> next) {
                 this.item = element;
                 this.next = next;
             }
         }
 
-        public void add(E item){
+        public void add(E item) {
             // 1、默认在尾部追加
             // 2、将游标移动至尾部
-            // 3、
+            // 3、新增的节点变为尾部节点
+//            Node<E> newNode = new Node<>(item, null);
+//            last.next = newNode;
+//            last = newNode;
+        }
 
-
+        @Override
+        public void delete(E e) {
+            // 1、先找到存储数据的节点
+            // 2、然后执行删除操作
 
         }
+
+        @Override
+        public void query(E e) {
+
+        }
+
+        @Override
+        public void foreach() {
+            if (first == null) {
+                System.out.println("空链表");
+            } else {
+                Node<E> currentNode = first;
+                System.out.println("first " + first.item);
+                while (currentNode.next != null) {
+                    currentNode = currentNode.next;
+                    System.out.println("currentNode " + currentNode.item);
+                }
+            }
+        }
+
+
     }
 
 
     public static void main(String[] args) {
 
+        MySingleLinkedList<String> mySingleLinkedList = new MySingleLinkedList<>(10);
+
+        mySingleLinkedList.foreach();
 
     }
 
