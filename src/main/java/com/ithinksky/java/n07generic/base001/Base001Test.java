92bd4a257b078502f80a6073ef117fac8d291a4d
tengpeng.gao <tengpeng.gao@gmail.com>
2020-05-18 18:09:34 +0800

    T 包含泛型转换

--- src/main/java/com/ithinksky/java/n07generic/base001/Base001Test.java
+++ src/main/java/com/ithinksky/java/n07generic/base001/Base001Test.java
@@ -1,5 +1,9 @@
 package com.ithinksky.java.n07generic.base001;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author tengpeng.gao
  */
@@ -19,9 +23,35 @@ public class Base001Test {
 	}
 
 	public static void main(String[] args) {
-		SingleSearchResultConvert convert = new SingleSearchResultConvert();
-		String test = Base001Test.queryForList("名称啊", convert, String.class);
-		System.out.println(" test ----- " + test);
+		SingleSearchResultConvert searchResultConvert = new SingleSearchResultConvert();
+		String test = Base001Test.queryForList("名称啊", searchResultConvert, String.class);
+		System.out.println(" test 单个值转换器----- " + test);
+
+		ListSearchResultConvert listSearchResultConvert = new ListSearchResultConvert();
+		List<String> list = Base001Test.queryForList("张三,李四,王五", listSearchResultConvert, List.class);
+		System.out.println(" test 列表值转换器----- " + list);
+
+	}
+}
+
+class ListSearchResultConvert implements SearchResultConvert<List<String>> {
+	@Override
+	public List<String> convert(String name) {
+
+		if (name == null || "".equals(name)) {
+			return Collections.EMPTY_LIST;
+		}
+		String[] names = name.split(",");
+		if (names == null || names.length <= 0) {
+			return Collections.EMPTY_LIST;
+		}
+
+		int size = names.length;
+		ArrayList<String> result = new ArrayList(size);
+		for (int i = 0; i < size; i++) {
+			result.add("列表值转换器" + names[i]);
+		}
+		return result;
 	}
 }
 
